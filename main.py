1d2fe82664d3ed7b9c88331e174acc079d5b87d8
D1935827V <dvestal2102@gmail.com>
2020-10-29 12:17:32 -0700

    group members

--- main.py
+++ main.py
@@ -11,5 +11,21 @@ def index():
 def aboutdane():
     return render_template('aboutdane.html')
 
+@app.route('/abouteva')
+def abouteva():
+    return render_template('abouteva.html')
+
+@app.route('/aboutida')
+def aboutida():
+    return render_template('aboutida.html')
+
+@app.route('/aboutcrystal')
+def aboutcrystal():
+    return render_template('aboutcrystal.html')
+
+@app.route('/aboutnivu')
+def aboutnivu():
+    return render_template('aboutnivu.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port='3000')
