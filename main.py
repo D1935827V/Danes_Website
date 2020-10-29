acb9898e71f81d26d4ee2b527a56098fb4047645
D1935827V <dvestal2102@gmail.com>
2020-10-29 09:52:49 -0700

    good

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/aboutdane')
+def aboutdane():
+    return render_template('aboutdane.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port='3000')
